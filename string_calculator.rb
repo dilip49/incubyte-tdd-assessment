80e8c51257bde577360a1c8c92e6d999311a0adc
Dilip Bhaidiya <dilip.bhaidiya@gmail.com>
2024-10-29 00:57:09 +0530

    Update the code to handle custom delimiters specified in the format //[delimiter]\n[numbers]

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,12 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+    
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers[2..].split("\n", 2)
+      numbers.split(Regexp.union(delimiter, "\n")).map(&:to_i).sum
+    else
+      numbers.split(/,|\n/).map(&:to_i).sum
+    end
   end
 end
