cbf1399884d6f88714104685d479b1398ee233e7
Dilip Bhaidiya <dilip.bhaidiya@gmail.com>
2024-11-06 16:03:49 +0530

    Add support for * as delimeter test

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,40 +3,41 @@
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
   describe '#add' do
     it 'returns 0 for an empty string' do
-      calculator = StringCalculator.new
       expect(calculator.add('')).to eq(0)
     end
 
     it 'returns the number itself when given a single number' do
-      calculator = StringCalculator.new
       expect(calculator.add('1')).to eq(1)
     end
 
     it 'returns the sum of two numbers separated by a comma' do
-      calculator = StringCalculator.new
       expect(calculator.add('1,2')).to eq(3)
     end
 
     it 'returns the sum of multiple comma-separated numbers' do
-      calculator = StringCalculator.new
       expect(calculator.add('1,2,3,4')).to eq(10)
     end
 
     it 'returns the sum of numbers separated by commas or new lines' do
-      calculator = StringCalculator.new
       expect(calculator.add("1\n2,3")).to eq(6)
     end
 
     it 'supports different delimiters when specified' do
-      calculator = StringCalculator.new
       expect(calculator.add("//;\n1;2")).to eq(3)
     end
 
     it 'raises an exception for a negative number' do
-      calculator = StringCalculator.new
       expect { calculator.add('1,-2,3') }.to raise_error('negative numbers not allowed: -2')
+    end
+
+    context 'when custom delimeters is start(*)' do
+      it 'returns the multiplication of numbers' do
+        expect(calculator.add("//*\n3*2")).to eq(6)
+      end
     end
   end
 end
